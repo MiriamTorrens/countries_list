76853d4e10570140d36273fa37b4ab721deee100
Pedro Lopez Mareque <pedro.lopez@clarity.ai>
2022-08-04 17:10:12 +0200

    refactor: use regex instead of splits and clean code

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const fs = require('fs');
 
-const countries = fs.readFileSync("countries.txt", { encoding: 'utf8', flag: 'r' }).split("\n");
+const countries = fs
+	.readFileSync("countries.txt", { encoding: 'utf8', flag: 'r' })
+	.split("\n");
 
-//Eliminar el primer elemento del array: "Country Population Area"
 countries.shift(); 
 
-//Crear un array de países=objetos para ordenarlo por la propiedad densidad
 const countriesList =
-    //Añadir cabecera de la lista
-    "Country,Population,Area,Density\n"+
     countries
-        //Dividir cada elemento del array en objeto: nombre, poblacion, area, densidad
-        .map(c => {
-            const splitData = c.split(" ")
-            const name = splitData.splice(0, (splitData.length - 2)).join().replace(/,/g, ' ');
-            const population = parseFloat(splitData[splitData.length - 2]?.replace(/,/g, ''));
-            const area = parseFloat(splitData[splitData.length - 1]?.replace(/,/g, ''));
+        .map(country => {
+	    // The regex search for the firsts not digits until the space,
+	    // then get the area and population numbers. 
+	    const { name, numbers } = /(?<name>[^\d]*)\s+(?<numbers>.*)/.exec(country).groups
+	    const [ population, area ] = numbers.split(" ").map(n => n.replace(",", ""));
             
-            const country = {
-                name: name,
-                population: population,
-                area: area,
-                density: population / area
+            return  {
+                name,
+                population,
+                area,
+                density: parseFloat(population) / parseFloat(area)
             }
-            return country;
         })
-        //Ordenar el array por densidad de población
         .sort((a, b) => b.density - a.density)
-        //Convertir el objeto country y el array countriesList en string para crear archivo csv
-        .map(c => Object.values(c).toString()).join("\n");
+        .map(c => `${Object.values(c)}`)
 
-fs.writeFileSync("countries.csv", countriesList);
+const csv = [ "Country,Population,Area,Density", ...countriesList].join("\n");
+fs.writeFileSync("countries.csv", csv);
